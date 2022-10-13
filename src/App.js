4af5ede3b4299983a5d643724c4d507fda266d5d
Hao Ngo <ngoanhhao24@gmail.com>
2022-10-13 18:22:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
+    return (
     <div className="App">
+        <h1 className="title">test</h1>
     </div>
-  );
+    )
 }
 
 export default App;
---
+++ src/components/GlobalStyle/index.js
@@ -0,0 +1,9 @@
+import './GlobalStyles.scss';
+
+function GlobalStyles({ children }) {
+    return (
+        <h1>test</h1>
+    )
+}
+
+export default GlobalStyles;
